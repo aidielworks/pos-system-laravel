a232440c8c90bddcb46c233ab150a7021dfd2f5c
Aidiel Daniel <aidiel@talentcloud.ai>
2024-02-02 14:55:17 +0800

    company module

---
+++ app/Models/CompanyModule.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyModule extends Model
+{
+    use HasFactory;
+
+    const MODULE = [
+        ''
+    ];
+}
